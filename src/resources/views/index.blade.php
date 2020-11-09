9d5c0b152e3b0412cd3bdac54549f4350e50cc54
yuziem14 <yuri.fnunes@gmail.com>
2020-11-08 21:04:17 -0300

    refactor: extend master layout

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,47 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="/images/favicon.svg" type="image/svg+xml">
+@section('styles')
+  <link rel="stylesheet" href="/css/main.css">
+@endsection
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="/css/main.css">
-
-    <title>{{env('APP_NAME')}}</title>
-  </head>
-  <body>
-    <div id="root">
-      <div id="page-main">
-        <aside class="container">
-          <img src="/images/logo.svg" alt="Hermes | Logo">
-          <section>
-            <img src="/images/wordmark.svg" alt="Hermes | Logo">
-            <h1>Hermes é o marketplace<span class="breakline"></span>para vender e comprar<span class="breakline"></span>diversos produtos.</h1>
-          </section>
-        </aside>
-        <main>
-          <div class="container">
-            <img src="/images/hero.svg" alt="Hero Image">
-            <div class="actions">
-              <a href="/login">
-                <p>Entrar</p>
-              </a>
-              <a href="/register">
-                <p>Registrar-se</p>
-              </a>
-            </div>
-          </div>
-        </main>
+@section('content')
+  <div id="page-main">
+    <aside class="container">
+      <img src="/images/logo.svg" alt="Hermes | Logo">
+      <section>
+        <img src="/images/wordmark.svg" alt="Hermes | Logo">
+        <h1>Hermes é o marketplace<span class="breakline"></span>para vender e comprar<span class="breakline"></span>diversos produtos.</h1>
+      </section>
+    </aside>
+    <main>
+      <div class="container">
+        <img src="/images/hero.svg" alt="Hero Image">
+        <div class="actions">
+          <a href="/login">
+            <p>Entrar</p>
+          </a>
+          <a href="/register">
+            <p>Registrar-se</p>
+          </a>
+        </div>
       </div>
-    </div>
-
-    <script src="/js/app.js" defer></script>
-  </body>
-</html>
+    </main>
+  </div>
+@endsection
