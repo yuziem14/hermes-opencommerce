4f3fa185ab8419b085ded3b11c079cd4bbf07b93
yuziem14 <yuri.fnunes@gmail.com>
2020-11-10 16:10:21 -0300

    chore: rename parcials folder to partials

--- src/resources/views/parcials/topbar.blade.php
+++
@@ -1,9 +0,0 @@
-<header id="top-bar">
-  <div>
-    <a href="{{ route('home') }}">
-      <img src="/images/arrow.svg" alt="Go Back">
-    </a>
-    <h1>{{ $pageName ?? '' || '' }}</h1>
-    <img src="/images/wordmark.svg" alt="Hermes | Wordmark">
-  </div>
-</header>
