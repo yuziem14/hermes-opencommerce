99acb379814f25e666b748975a6b0fff92fc8e2c
yuziem14 <yuri.fnunes@gmail.com>
2020-11-09 23:46:31 -0300

    bugfix: remove id param for logout controller method

--- src/app/Http/Controllers/Auth/SessionController.php
+++ src/app/Http/Controllers/Auth/SessionController.php
@@ -44,7 +44,7 @@ class SessionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy()
     {
         Auth::logout();
 
