17bf4ca864a84fa50ed714835ec3c5d5fac83ea6
yuziem14 <yuri.fnunes@gmail.com>
2020-11-10 17:22:01 -0300

    chore: create products and images table via migration

---
+++ src/database/migrations/2020_11_10_194154_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->double('initial_price', 8, 2);
+            $table->double('price', 8, 2);
+            $table->integer('amount')->default(1);
+            $table->mediumText('description')->nullable();
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+              ->references('id')
+              ->on('users')
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
