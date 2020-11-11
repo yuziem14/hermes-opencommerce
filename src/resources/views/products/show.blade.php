a38b82b217c0cd503ec11bc1db3e1806b797034f
yuziem14 <yuri.fnunes@gmail.com>
2020-11-10 23:43:23 -0300

    chore: create product page

---
+++ src/resources/views/products/show.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.master')
+
+@php
+  $pageName = $product->name;
+@endphp
+
+@section('styles')
+  <link rel="stylesheet" href="/css/partials.css">
+  <link rel="stylesheet" href="/css/product-page.css">
+@endsection
+
+@section('content')
+  <div id="page-product">
+    @include('partials.topbar')
+    <main>
+      @include('partials.navbar')
+
+      <aside id="aside-content" class="container with-nav">
+        <div class="container">
+          <header>
+            <h1>{{ $product->name }}</h1>
+            <p>{{ $product->description }}</p>
+            <p>Incluido em: {{
+                implode(', ', array_column($product->tags->toArray(), 'name'))
+              }}
+            </p>
+          </header>
+          <main>
+            <div class="product-images">
+              @foreach($product->images as $image)
+                <img
+                  src="/uploads/products/{{ $image->filename }}"
+                  alt="Product Image"
+                />
+              @endforeach
+            </div>
+          </main>
+          <footer>
+            <div class="info">
+              <p>por<span>R$ {{ number_format($product->price, 2, ',', '.') }}</span></p>
+              <p>Há <span>{{ $product->amount }}</span> disponíveis</p>
+            </div>
+            @if(Auth::user()->id === $product->user->id)
+              <div class="actions">
+                {{-- <a class="action" href="{{ route('products.edit', $product->id) }}">
+                  <img src="/images/edit.svg" alt="Edit" />
+                  <p>Editar</p>
+                </a> --}}
+                <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                  @method('DELETE')
+                  @csrf
+                  <button type="submit" class="action">
+                    <img src="/images/trash.svg" alt="Delete">
+                    <p>
+                      Excluir
+                    </p>
+                  </button>
+                </form>
+              </div>
+            @endif
+          </footer>
+        </div>
+      </aside>
+    </main>
+  </div>
+@endsection
+
+@section('scripts')
+@endsection
